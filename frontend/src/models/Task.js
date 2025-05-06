db156da6f1595ea3122be97d060d69233afdf0b0
mahimnamehta <mahimna.mehta99@gmail.com>
2025-05-06 14:04:18 -0700

    frontend

--- frontend/src/models/Task.js
+++
@@ -1,9 +0,0 @@
-// src/models/Task.js
-
-/**
- * @typedef {Object} Task
- * @property {number} [id]
- * @property {string} title
- * @property {string} description
- * @property {boolean} completed
- */
